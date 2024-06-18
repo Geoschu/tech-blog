851843bf96563c040b38fc556722cd36b44c952f
George Schultz <georgeschultz@outlook.com>
2024-06-17 19:28:48 -0700

    reworked login and logout

--- public/js/login.js
+++ public/js/login.js
@@ -1,53 +1,61 @@
-const loginFormHandler = async (event) => {
+// Login form handler
+const handleLogin = async (event) => {
   event.preventDefault();
 
-  // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  // Collect form values
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      // Redirect to the dashboard if the login is successful
+      document.location.replace("/dashboard");
     } else {
-      alert(response.statusText);
+      console.error(response.statusText);
+      // Helper function to display error message
+      toggleMessage(
+        "login-error",
+        "That was the incorrect email and/or password, please try again."
+      );
     }
   }
 };
 
-const signupFormHandler = async (event) => {
+// Signup form handler
+const handleSignup = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  // Collect form values
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      // Redirect to the dashboard if the signup successful
+      document.location.replace("/dashboard");
     } else {
-      alert(response.statusText);
+      console.error(response.statusText);
+      // Helper function to display the error message
+      toggleMessage(
+        "signup-error",
+        "There was an issue creating your account, please try again."
+      );
     }
   }
 };
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+// Event listeners
+document.querySelector(".login-form").addEventListener("submit", handleLogin);
+document.querySelector(".signup-form").addEventListener("submit", handleSignup);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,16 @@
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+// Logout handling
+const handleLogout = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace("/");
   } else {
-    alert(response.statusText);
+    console.error(response.statusText);
   }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+// Event listener
+document.querySelector("#logout").addEventListener("click", handleLogout);
